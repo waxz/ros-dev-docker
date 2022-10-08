cb7e99a5b5292ee497862c7cf78b8163d4848a92
wonder <waxz@users.noreply.github.com>
2022-10-08 18:51:45 +0800

    add cartographer_ros

--- shell/entrypoint.sh
+++ shell/entrypoint.sh
@@ -2,7 +2,7 @@
 source /opt/ros/$ROS_DISTRO/setup.bash
 #echo 1234 | sudo -S  sh -c "sudo ln -s /opt/cmake/bin/* /usr/local/bin/"
 cmake --version
-echo "please input password to start ssh server"
-sudo /usr/sbin/sshd -p 2222
+echo "please input password to start ssh server , using port $SSH_PORT "
+sudo /usr/sbin/sshd -p $SSH_PORT
 tmux
 
